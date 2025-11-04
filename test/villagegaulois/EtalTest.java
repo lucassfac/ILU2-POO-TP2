6095ec0b7f2565c1f70f65dd9918f0137f56f4ed
lucassfac <lucas.de-oliveira-costa@univ-tlse3.fr>
2025-11-04 11:55:56 +0100

    TP TEST

---
+++ test/villagegaulois/EtalTest.java
@@ -0,0 +1,55 @@
+package villagegaulois;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EtalTest {
+
+	@Test
+	void testIsEtalOccupe() {
+		Etal etal = new Etal(); // assuming constructor sets etalOccupe
+        assertTrue(etal.isEtalOccupe(), "Etal should be marked as occupied");
+	}
+
+	@Test
+	void testGetVendeur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetQuantite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOccuperEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContientProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLibererEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEtatEtal() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/villagegaulois/VillageTest.java
@@ -0,0 +1,74 @@
+package villagegaulois;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VillageTest {
+
+	@Test
+	void testVillage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetChef() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAjouterHabitant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTrouverHabitant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDonnerVillageois() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDonnerNbEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInstallerVendeur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPartirVendeur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRechercherEtalVide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRechercherVendeursProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRechercherEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDonnerEtatMarche() {
+		fail("Not yet implemented");
+	}
+
+}
