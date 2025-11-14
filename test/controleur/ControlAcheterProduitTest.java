23fe4719178d4f8423af80249f4861c108eb9f45
lucassfac <lucas.de-oliveira-costa@univ-tlse3.fr>
2025-11-14 11:53:25 +0100

    TP3 TEST

---
+++ test/controleur/ControlAcheterProduitTest.java
@@ -0,0 +1,14 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControlAcheterProduitTest {
+
+	@Test
+	void testControlAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+}
--- test/villagegaulois/EtalTest.java
+++ test/villagegaulois/EtalTest.java
@@ -2,39 +2,78 @@ package villagegaulois;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.im.InputContext;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EtalTest {
+import personnages.Gaulois;
 
+class EtalTest {
+	private Gaulois vendeur;
+	private String produit;
+	private int quantiteDebutMarche;
+	private int quantite;
+	private boolean etalOccupe = false;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("INIT");
+		Etal etal = new Etal();
+		Gaulois gaul = new Gaulois("gaul", 6);
+		etal.occuperEtal(gaul, "pommes", 6);
+	}
+	
 	@Test
 	void testIsEtalOccupe() {
 		Etal etal = new Etal(); // assuming constructor sets etalOccupe
-        assertTrue(etal.isEtalOccupe(), "Etal should be marked as occupied");
+		Gaulois expresult = new Gaulois("gaul", 6);
+		etal.occuperEtal(expresult, "pommes", 6);
+		assertTrue(etal.isEtalOccupe());
 	}
 
 	@Test
 	void testGetVendeur() {
-		fail("Not yet implemented");
+		Etal etal = new Etal();
+		Gaulois expresult = new Gaulois("gaul", 6);
+		etal.occuperEtal(expresult, "pommes", 6);
+		Gaulois result = etal.getVendeur();
+		assertEquals(expresult, result);
 	}
 
 	@Test
 	void testGetQuantite() {
-		fail("Not yet implemented");
+		int expresult = 6;
+		Etal etal = new Etal();
+		Gaulois tmpGaul = new Gaulois("gaul", 6);
+		etal.occuperEtal(tmpGaul, "pommes", 6);
+		assertEquals(expresult, etal.getQuantite());
 	}
 
 	@Test
 	void testGetProduit() {
-		fail("Not yet implemented");
+		String expresult = "pommes";
+		Etal etal = new Etal();
+		Gaulois tmpGaul = new Gaulois("gaul", 6);
+		etal.occuperEtal(tmpGaul, "pommes", 6);
+		assertEquals(expresult, etal.getProduit());
 	}
 
 	@Test
 	void testOccuperEtal() {
-		fail("Not yet implemented");
+		testGetVendeur();
+		testGetQuantite();
+		testGetProduit();
 	}
 
 	@Test
 	void testContientProduit() {
-		fail("Not yet implemented");
+		String produit = "pommes";
+		Etal etal = new Etal();
+		Gaulois tmpGaul = new Gaulois("gaul", 6);
+		etal.occuperEtal(tmpGaul, "pommes", 6);
+		this.produit = produit;
+		assertEquals(this.produit, produit);
 	}
 
 	@Test
